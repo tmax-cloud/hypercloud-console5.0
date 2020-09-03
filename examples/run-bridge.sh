e8700fa0d212829c63f4f96ed93333e8a740e90a
신은경 <eunkyeng_shin@tmax.co.kr>
2020-09-03 15:19:39 +0900

    local주소수정

--- examples/run-bridge.sh
+++ examples/run-bridge.sh
@@ -3,10 +3,10 @@
 set -exuo pipefail
 
 ./bin/bridge \
-    --base-address=http://localhost:9000 \
+    --base-address=http://172.23.0.2:9000 \
     --ca-file=examples/ca.crt \
     --k8s-mode=off-cluster \
-    --listen=http://127.0.0.1:9000 \
+    --listen=http://172.23.0.2:9000 \
     --k8s-mode-off-cluster-endpoint=https://172.22.6.2:6443 \
     --public-dir=./frontend/public/dist \
     --k8s-auth-bearer-token=@@ \
