b5c346fdb5d751f36476c77d8fd0c00ee2e61b82
hyejin_park <hyejinp128@naver.com>
2020-12-01 16:05:12 +0900

    [feat] 페더레이션 모델 추가

--- frontend/public/models/hypercloud/index.ts
+++ frontend/public/models/hypercloud/index.ts
@@ -26,97 +26,157 @@ export const HyperClusterResourceModel: K8sKind = {
 };
 
 export const FederatedConfigMapModel: K8sKind = {
-  label: 'Config Map',
-  labelPlural: 'Config Maps',
+  label: 'Federated Config Map',
+  labelPlural: 'Federated Config Maps',
   apiVersion: 'v1beta1',
   apiGroup: 'types.kubefed.io',
   plural: 'federatedconfigmaps',
-  abbr: 'FC',
+  abbr: 'FCM',
   kind: 'FederatedConfigMap',
   id: 'federatedconfigmap',
   namespaced: true,
 };
 
 export const FederatedDeploymentModel: K8sKind = {
-  label: 'Deployment',
-  labelPlural: 'Deployments',
+  label: 'Federated Deployment',
+  labelPlural: 'Federated Deployments',
   apiVersion: 'v1beta1',
   apiGroup: 'types.kubefed.io',
   plural: 'federateddeployments',
-  abbr: 'FC',
+  abbr: 'FDEPLOY',
   kind: 'FederatedDeployment',
   id: 'federateddeployment',
   namespaced: true,
 };
 
 export const FederatedIngressModel: K8sKind = {
-  label: 'Ingress',
-  labelPlural: 'Ingresses',
+  label: 'Federated Ingress',
+  labelPlural: 'Federated Ingresses',
   apiVersion: 'v1beta1',
   apiGroup: 'types.kubefed.io',
   plural: 'federatedingresses',
-  abbr: 'I',
+  abbr: 'FING',
   kind: 'FederatedIngress',
   id: 'federatedingress',
   namespaced: true,
 };
 
 export const FederatedJobModel: K8sKind = {
-  label: 'Job',
-  labelPlural: 'Jobs',
+  label: 'Federated Job',
+  labelPlural: 'Federated Jobs',
   apiVersion: 'v1beta1',
   apiGroup: 'types.kubefed.io',
   plural: 'federatedjobs',
-  abbr: 'J',
+  abbr: 'FJ',
   kind: 'FederatedJob',
   id: 'federatedjob',
   namespaced: true,
 };
 
 export const FederatedNamespaceModel: K8sKind = {
-  label: 'Namespace',
-  labelPlural: 'Namespaces',
+  label: 'Federated Namespace',
+  labelPlural: 'Federated Namespaces',
   apiVersion: 'v1beta1',
   apiGroup: 'types.kubefed.io',
   plural: 'federatednamespaces',
-  abbr: 'N',
+  abbr: 'FNS',
   kind: 'FederatedNamespace',
   id: 'federatednamespace',
   namespaced: true,
 };
 
 export const FederatedReplicaSetModel: K8sKind = {
-  label: 'Replica Set',
-  labelPlural: 'Replica Sets',
+  label: 'Federated Replica Set',
+  labelPlural: 'Federated Replica Sets',
   apiVersion: 'v1beta1',
   apiGroup: 'types.kubefed.io',
   plural: 'federatedreplicasets',
-  abbr: 'R',
+  abbr: 'FRS',
   kind: 'FederatedReplicaSet',
   id: 'federatedreplicaset',
   namespaced: true,
 };
 
 export const FederatedSecretModel: K8sKind = {
-  label: 'Secret',
-  labelPlural: 'Secrets',
+  label: 'Federated Secret',
+  labelPlural: 'Federated Secrets',
   apiVersion: 'v1beta1',
   apiGroup: 'types.kubefed.io',
   plural: 'federatedsecrets',
-  abbr: 'S',
+  abbr: 'FS',
   kind: 'FederatedSecret',
   id: 'federatedsecret',
   namespaced: true,
 };
 
 export const FederatedServiceModel: K8sKind = {
-  label: 'Service',
-  labelPlural: 'Services',
+  label: 'Federated Service',
+  labelPlural: 'Federated Services',
   apiVersion: 'v1beta1',
   apiGroup: 'types.kubefed.io',
   plural: 'federatedservices',
-  abbr: 'S',
+  abbr: 'FSVC',
   kind: 'FederatedService',
   id: 'federatedservice',
+  namespaced: true,
+};
+
+export const FederatedPodModel: K8sKind = {
+  label: 'Federated Pod',
+  labelPlural: 'Federated Pods',
+  apiVersion: 'v1beta1',
+  apiGroup: 'types.kubefed.io',
+  plural: 'federatedpods',
+  abbr: 'FPO',
+  kind: 'FederatedPod',
+  id: 'federatedpod',
+  namespaced: true,
+};
+
+export const FederatedHPAModel: K8sKind = {
+  label: 'Federated Horizontal Pod Autoscaler',
+  labelPlural: 'Federated Horizontal Pod Autoscalers',
+  apiVersion: 'v1beta1',
+  apiGroup: 'types.kubefed.io',
+  plural: 'federatedhorizontalpodautoscalers',
+  abbr: 'FHPA',
+  kind: 'FederatedHorizontalPodAutoscaler',
+  id: 'federatedhorizontalpodautoscaler',
+  namespaced: true,
+};
+
+export const FederatedDaemonSetModel: K8sKind = {
+  label: 'Federated Daemon Set',
+  labelPlural: 'Federated Daemon Sets',
+  apiVersion: 'v1beta1',
+  apiGroup: 'types.kubefed.io',
+  plural: 'federateddaemonsets',
+  abbr: 'FDS',
+  kind: 'FederatedDaemonSet',
+  id: 'federateddaemonset',
+  namespaced: true,
+};
+
+export const FederatedStatefulSetModel: K8sKind = {
+  label: 'Federated Stateful Set',
+  labelPlural: 'Federated Stateful Sets',
+  apiVersion: 'v1beta1',
+  apiGroup: 'types.kubefed.io',
+  plural: 'federatedstatefulsets',
+  abbr: 'FSTS',
+  kind: 'FederatedStatefulSet',
+  id: 'federatedstatefulset',
+  namespaced: true,
+};
+
+export const FederatedCronJobModel: K8sKind = {
+  label: 'Federated Cron Job',
+  labelPlural: 'Federated Cron Jobs',
+  apiVersion: 'v1beta1',
+  apiGroup: 'types.kubefed.io',
+  plural: 'federatedcronjobs',
+  abbr: 'FCJ',
+  kind: 'FederatedCronJob',
+  id: 'federatedcronjob',
   namespaced: true,
 };
