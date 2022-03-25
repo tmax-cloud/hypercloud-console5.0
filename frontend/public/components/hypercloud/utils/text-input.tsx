e7851c41b2dcae543a5cc1e8a41e6e06de400f94
gyoungjun_lee <injxgj@gmail.com>
2022-03-25 17:34:42 +0900

    [feat][patch] cherrypick 제외된 것들 추가

--- frontend/public/components/hypercloud/utils/text-input.tsx
+++ frontend/public/components/hypercloud/utils/text-input.tsx
@@ -3,9 +3,9 @@ import * as React from 'react';
 import * as classNames from 'classnames';
 import { useFormContext } from 'react-hook-form';
 
-export const TextInput: React.FC<TextInputProps> = ({ id, methods, inputClassName, defaultValue, placeholder, type, isDisabled = false, valid = true }) => {
+export const TextInput: React.FC<TextInputProps> = ({ id, methods, style, inputClassName, defaultValue, placeholder, type, isDisabled = false, valid = true }) => {
   const { register } = methods ? methods : useFormContext();
-  return <input className={classNames(inputClassName, { ['error-text']: !valid })} placeholder={placeholder} defaultValue={defaultValue} name={id} ref={register()} disabled={isDisabled} type={type}></input>;
+  return <input className={classNames(inputClassName, { ['error-text']: !valid })} style={style} placeholder={placeholder} defaultValue={defaultValue} name={id} ref={register()} disabled={isDisabled} type={type}></input>;
 };
 
 type TextInputProps = {
